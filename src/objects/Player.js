ecb26e1bb548e5634d5149405190b90cff0efd5b
simon <nikolasimonovic94@gmail.com>
2021-02-28 01:35:19 +0100

    ⚡ Moved keypress logic from Player to index

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -5,36 +5,24 @@ export class Player {
     this.height = height;
     this.depth = depth;
     this.position = position;
-    this.isMovingLeft = false;
-    this.isMovingRight = false;
-    document.addEventListener("keydown", this.handleKeyDown);
-    document.addEventListener("keyup", this.handleKeyUp);
+    this.moveLeft = false;
+    this.moveRight = false;
+    this._obj = null;
   }
 
-  move() {
-    if (this.isMovingLeft) {
-      this.position.x -= this.speed;
-    }
-    if (this.isMovingRight) {
-      this.position.x += this.speed;
-    }
+  get isMovingLeft() {
+    return this.moveLeft;
   }
 
-  handleKeyDown(e) {
-    if (e.code === "ArrowLeft" || e.key === "ArrowLeft") {
-      this.moveLeft = true;
-    }
-    if (e.code === "ArrowRight" || e.key === "ArrowRight") {
-      this.moveRight = true;
-    }
+  get isMovingRight() {
+    return this.moveRight;
   }
 
-  handleKeyUp(e) {
-    if (e.code === "ArrowLeft" || e.key === "ArrowLeft") {
-      this.moveLeft = false;
-    }
-    if (e.code === "ArrowRight" || e.key === "ArrowRight") {
-      this.moveRight = false;
-    }
+  set isMovingLeft(moveLeft) {
+    this.moveLeft = moveLeft;
+  }
+
+  set isMovingRight(moveRight) {
+    this.moveRight = moveRight;
   }
 }
