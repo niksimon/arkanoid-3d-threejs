83d1ddc15af94e665c492ad473b75fbd4d9560ec
simon <nikolasimonovic94@gmail.com>
2021-02-28 01:36:17 +0100

    ⚡ Moving player logic and rendering it on screen

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import * as SETUP from "./constants";
 import { Player } from "./objects/Player";
 
+// Scene, camera, renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 150);
 
@@ -9,34 +10,77 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(SETUP.BRICK_WIDTH, SETUP.BRICK_HEIGHT, SETUP.BRICK_DEPTH);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-
 // Player
-const player = new Player(0.1, 3, 1, 1, { x: 0, y: 0, z: 0 });
+const player = new Player(0.2, 3, 1, 1, { x: SETUP.SCREEN_MID, y: 0, z: 15 });
+const playerObj = new THREE.Mesh(
+  new THREE.BoxGeometry(player.width, player.height, player.depth),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+playerObj.position.set(player.position.x, player.position.y, player.position.z);
+
+// Player move handle
+const movePlayer = () => {
+  if (player.moveLeft) {
+    playerObj.position.setX(playerObj.position.x - player.speed);
+  }
+  if (player.moveRight) {
+    playerObj.position.setX(playerObj.position.x + player.speed);
+  }
+};
+
+const handleKeyDown = (e) => {
+  if (e.code === "ArrowLeft" || e.key === "ArrowLeft") {
+    player.isMovingLeft = true;
+  }
+  if (e.code === "ArrowRight" || e.key === "ArrowRight") {
+    player.isMovingRight = true;
+  }
+};
+
+const handleKeyUp = (e) => {
+  if (e.code === "ArrowLeft" || e.key === "ArrowLeft") {
+    player.isMovingLeft = false;
+  }
+  if (e.code === "ArrowRight" || e.key === "ArrowRight") {
+    player.isMovingRight = false;
+  }
+};
+
+document.addEventListener("keydown", handleKeyDown);
+document.addEventListener("keyup", handleKeyUp);
+
+// Add player to scene
+scene.add(playerObj);
 
 // Bricks
+const brickGeometry = new THREE.BoxGeometry(SETUP.BRICK_WIDTH, SETUP.BRICK_HEIGHT, SETUP.BRICK_DEPTH);
+const brickMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
 for (let i = 0; i < SETUP.BRICKS_ROWS; i++) {
   for (let j = 0; j < SETUP.BRICKS_COLS; j++) {
-    const brick = new THREE.Mesh(geometry, material);
+    const brick = new THREE.Mesh(brickGeometry, brickMaterial);
     brick.position.set(j * (SETUP.BRICK_WIDTH + SETUP.BRICK_PADDING), 0, i * (SETUP.BRICK_DEPTH + SETUP.BRICK_PADDING));
     scene.add(brick);
   }
 }
 
-camera.position.x =
-  (SETUP.BRICKS_COLS * SETUP.BRICK_WIDTH + SETUP.BRICKS_COLS * BRICK_PADDING) / 2 -
-  SETUP.BRICK_WIDTH / 2 -
-  SETUP.BRICK_PADDING / 2;
-camera.position.y = 5;
-camera.position.z = 20;
+camera.position.x = SETUP.SCREEN_MID;
+camera.position.y = 10;
+camera.position.z = 26;
 
-camera.rotation.x = -0.5;
+camera.rotation.x = -0.6;
 camera.rotation.y = 0;
 camera.rotation.z = 0;
 
 const animate = function () {
   requestAnimationFrame(animate);
+
+  if (
+    (player.isMovingLeft && playerObj.position.x >= SETUP.BOUNDARY_LEFT) ||
+    (player.isMovingRight && playerObj.position.x <= SETUP.BOUNDARY_RIGHT - player.width + SETUP.BRICK_PADDING)
+  ) {
+    movePlayer();
+  }
 
   renderer.render(scene, camera);
 };
